7c8bf2dc5d9e836edb4ab80c02ee62b636274c56
Jason Smith (MacBook) <jason.h.smith@gmail.com>
2016-09-15 15:54:44 +0700

    More playing with the UI

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,45 +1,38 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ionic', 'ngCordova'])
 
 .controller('DashCtrl', function($scope, $timeout) {
   $scope.settings = { enableMonitoring: false, city:'Boston' }
+  $scope.syncStatus = 'off'
+
   $scope.changeCity = function() {
     console.log('TODO: City changed', $scope.settings)
   }
 
   $scope.changeMonitoring = function() {
     console.log('Monitoring setting changed', $scope.settings.enableMonitoring)
-    $scope.status = 'sync'
+    $scope.syncStatus = 'sync'
     $timeout(sync_done, 3000)
   }
 
   function sync_done() {
-    $scope.status = 'ok'
-    console.log('sync done $scope.city = ', $scope.city)
+    $scope.syncStatus = 'ok'
   }
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ChatsCtrl', function($scope, $cordovaGeolocation) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
   //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  console.log('The scope is now', $scope.settings)
+  $scope.$on('$ionicView.enter', function(e) {
+    console.log('Enter view', e)
+  });
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+  window.g = $cordovaGeolocation
+  console.log('check "g"')
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
